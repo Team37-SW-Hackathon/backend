de55d98e748d9085932a4380c1dff309b20909bb
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-06-29 02:42:36 +0900

    init setting

---
+++ src/main/java/SWHackathonTeam37/SimplePrompt/SimplePromptApplication.java
@@ -0,0 +1,13 @@
+package SWHackathonTeam37.SimplePrompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SWHackathonTeam37/SimplePrompt/SimplePromptApplicationTests.java
@@ -0,0 +1,13 @@
+package SWHackathonTeam37.SimplePrompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

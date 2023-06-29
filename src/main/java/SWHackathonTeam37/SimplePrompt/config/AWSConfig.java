971d03877be59e236dc6f72d2ffa3a219e16f292
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-06-30 04:23:49 +0900

    [fix] AWSConfig를 되돌려 놓는다

--- src/main/java/SWHackathonTeam37/SimplePrompt/config/AWSConfig.java
+++ src/main/java/SWHackathonTeam37/SimplePrompt/config/AWSConfig.java
@@ -1,25 +1,41 @@
-//package SWHackathonTeam37.SimplePrompt.config;
-//
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.services.s3.AmazonS3Client;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class AWSConfig {
-//    private final String accessKey = ApiKey.getInstance().getAwsAccessKey();
-//    private final String accessSecret = ApiKey.getInstance().getAwsSecretKey();
-//    private final String region = ApiKey.getInstance().getAwsRegion();
-//
-//    @Bean
-//    public AmazonS3Client amazonS3Client() {
-//        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, accessSecret);
-//        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-//                .withRegion(region)
-//                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-//                .build();
-//    }
-//}
+package SWHackathonTeam37.SimplePrompt.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+    private String accessKey;
+    private String accessSecret;
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, accessSecret);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+
+    // 여기 아래부분에 Value부분만 맞게 조절하시면 됩ㄴ디ㅏ
+    @Value("${aws-access-key}")
+    public void setAccessKey(String value) {
+        accessKey = value;
+    }
+
+    @Value("${aws-secret-key}")
+    public void setAccessSecret(String value) {
+        accessSecret = value;
+    }
+
+    @Value("${aws-region}")
+    public void setRegion(String value) {
+        region = value;
+    }
+}

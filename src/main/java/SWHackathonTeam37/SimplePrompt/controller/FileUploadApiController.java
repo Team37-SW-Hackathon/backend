b6d78f89a79fe6bf833e862a1c41a67129dd65fe
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-06-30 03:13:19 +0900

    [fix] 파일업로드 controller, service를 돌려놓는다

--- src/main/java/SWHackathonTeam37/SimplePrompt/controller/FileUploadApiController.java
+++ src/main/java/SWHackathonTeam37/SimplePrompt/controller/FileUploadApiController.java
@@ -1,26 +1,27 @@
 package SWHackathonTeam37.SimplePrompt.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import SWHackathonTeam37.SimplePrompt.controller.dto.request.FileUploadRequest;
+import SWHackathonTeam37.SimplePrompt.service.FileUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/fileUpload")
 public class FileUploadApiController {
+    private final FileUploadService fileUploadService;
 
-    @Value("${file.dir}")
-    private String fileDir;
-
-    public void saveFile(@RequestParam MultipartFile file) throws IOException {
-        if (!file.isEmpty()) {
-            String fullPath = UUID.randomUUID().toString() + "_" + fileDir + file.getOriginalFilename();
-            file.transferTo(new File(fullPath));
-        }
-        return ;
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> fileUpload(@ModelAttribute @Valid FileUploadRequest request) throws IOException {
+        String fileUrl = fileUploadService.upload(request.multipartFile());
+        return ResponseEntity.ok(fileUrl);
     }
 }

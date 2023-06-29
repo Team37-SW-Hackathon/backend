1d02d7ac66587b18139fd59a782d10c0c46b52ff
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-06-30 05:54:50 +0900

    [feat] 문서화 함수를 구현하다 (#20)

--- src/main/java/SWHackathonTeam37/SimplePrompt/service/ExamService.java
+++ src/main/java/SWHackathonTeam37/SimplePrompt/service/ExamService.java
@@ -4,12 +4,19 @@ import SWHackathonTeam37.SimplePrompt.domain.ExamRepository;
 import SWHackathonTeam37.SimplePrompt.service.dto.response.ExamAssembler;
 import SWHackathonTeam37.SimplePrompt.service.dto.response.SimpleExam;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -28,64 +35,64 @@ public class ExamService {
         // 5. 클라우드에 파일 올리기
     }
 
-    public void workbook1(String doc){
+    public void createDocument(List<String> passageList, List<String> questionList) {
+        XWPFDocument document = new XWPFDocument();
 
-        List<String> workbook = new ArrayList<>();
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun run = paragraph.createRun();
 
-        workbook = doc.split(" ");
-
-        Collections.shuffle(workbook);
-
-        return workbook;
-    }
-
-    public void workbook2(String doc){
-
-        List<String> workbook = new ArrayList<>();
-
-        workbook = doc.split(".");
-
-        return workbook;
-
-    }
-
-    public void workbook3(String doc){
-
-        List<String> var = new ArrayList<>();
-
-        List<String> workbook = new ArrayList<>();
-        
-        int len = 0;
-
-        var = doc.split(".");
-
-        len = var.length;
-
-        if (len>1){
-            for(int i = 1 ; i < len ; i += 2){
-                workbook.add(var[i-1] + var[i]);
-            }
-        
-            if(len & 2 == 1){
-                workbook.add(var[-1]);
-            }
+        String result = "";
+        for (int i = 0; i < passageList.size(); i++) {
+            result += Integer.toString(i+1) + passageList.get(i) + "\n" + questionList.get(i);
         }
-        
-        Collections.shuffle(workbook);
 
-        return workbook;
+        run.setText(result);
+
+        String fileName = "시험지" + Math.random() * 10000 +".docx";
+        try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
+            document.write(outputStream);
+            System.out.println("Word 파일이 생성되었습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void uploadToAmazonS3() {
     public ExamAssembler getList(int subject) {
         Slice<SimpleExam> result = examRepository.findExamBySubject(subject);
         return new ExamAssembler(result.getContent(), result.hasNext());
     }
 
-
     public void deleteList(List<Long> idList) {
         for (Long id : idList) {
             examRepository.deleteById(id);
         }
+    }
+
+    public List<String> workbook1(String doc){
+        List<String> workbook = List.of(doc.split(" "));
+        Collections.shuffle(workbook);
+        return workbook;
+    }
+
+    public List<String> workbook2(String doc){
+        List<String> workbook = List.of(doc.split("."));
+        return workbook;
+    }
+
+    public List<String> workbook3(String doc){
+        List<String> var = List.of(doc.split("."));
+        int len = var.size();
+
+        List<String> workbook = new ArrayList<>();
+        if (len > 1) {
+            for(int i = 1 ; i < len ; i += 2){
+                workbook.add(var.get(i-1) + var.get(i));
+            }
+//            if(len & 2 == 1){
+//                workbook.add(var.get(-1));
+//            }
+        }
+        Collections.shuffle(workbook);
+        return workbook;
     }
 }
